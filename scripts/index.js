9985e557e345952acd12c85a5cf28e1298d18ba7
mbonifasi2019139 <69530122+mbonifasi2019139@users.noreply.github.com>
2020-08-20 19:41:42 -0600

    Trabajando en el Login y el Profile page

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+let btnLogin = document.getElementById('btn-login');
+let nameLogin = document.getElementById('name');
+let passwordLogin = document.getElementById('password');
+
+btnLogin.onclick = () => {
+    window.location.href = "../songs.html";
+}
